995493aaa48ef6500f6772a3a60a91b995734d2a
Luiz Tanure <letanure@gmail.com>
2023-08-23 23:06:04 +0200

    Enable PWA and minify code in production mode

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: isProd,
+  },
+}
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd,
+  register: true,
+})
+
+module.exports = withPWA(nextConfig)
