ae9cf06f0bc90019fbc77af11d5cef9a95d02978
Luiz Tanure <letanure@gmail.com>
2023-08-24 23:06:15 +0200

    Update Babel config, add eslintignore patterns, fix lint-staged script, and update Main component styles

--- src/app/components/Main/styles.ts
+++ src/app/components/Main/styles.ts
@@ -3,10 +3,11 @@ import styled from 'styled-components'
 export const Wrapper = styled.main`
   height: 100%;
   width: 100%;
+  background-color: #a2aecd;
 `
 
 export const Title = styled.h1`
-  color: #fff;
+  color: #333;
   font-size: 5rem;
   font-weight: 700;
   margin-bottom: 2rem;
--- src/app/components/Main/test.tsx
+++ src/app/components/Main/test.tsx
@@ -5,10 +5,19 @@ import Main from '.'
 describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
+
     expect(
       screen.getByRole('heading', { name: /Boilerplate/i }),
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it.skip('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': 'rgb(162, 174, 205)',
+    })
   })
 })
