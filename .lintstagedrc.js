e3e9ab895583f3072acacbc2656e0f3b61579d8d
Luiz Tanure <letanure@gmail.com>
2023-09-13 23:59:17 +0200

    chore: Add linting and testing configuration

---
+++ .lintstagedrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Lint & Prettify TS and JS files
+  "*.{js,jsx,ts,tsx}": (filenames) => [
+    `prettier --write ${filenames.join(" ")}`,
+    `pnpm lint --fix . ${filenames.join(" --file")}`,
+    `pnpm test -- --findRelatedTests ${filenames.join(" ")}`,
+  ],
+}
