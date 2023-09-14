05005ed5e8a24a27d5365aa7337b144ece051b73
Luiz Tanure <letanure@gmail.com>
2023-09-15 00:15:15 +0200

    feat(Main): add Basic story with custom title and description

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,16 +1,26 @@
 import type { Meta, StoryObj } from '@storybook/react'
+
 import Main from './'
 
 const meta: Meta<typeof Main> = {
+  title: 'Components/Main',
   component: Main,
   args: {
     title: 'Boilerplate',
     description: 'TypeScript, ReactJS, NextJS e Styled Components',
   },
+  parameters: {
+    layout: 'fullscreen',
+  },
 }
 
 export default meta
 
-type Story = StoryObj<typeof Main>
+export const Default: StoryObj<typeof Main> = {}
 
-export const Default: Story = {}
+export const Basic: StoryObj = {
+  args: {
+    title: 'title basic',
+    description: 'description basic',
+  },
+}
