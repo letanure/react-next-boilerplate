8e1bab89141c02ee5f034ca1fb47e5bd1dded98c
Luiz Tanure <letanure@gmail.com>
2023-09-14 21:23:29 +0200

    style: improve code formatting and linting configuration

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   // Lint & Prettify TS and JS files
-  "*.{js,jsx,ts,tsx}": (filenames) => [
-    `prettier --write ${filenames.join(" ")}`,
-    `pnpm lint --fix . ${filenames.join(" --file")}`,
-    `pnpm test -- --findRelatedTests ${filenames.join(" ")}`,
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`,
+    `pnpm lint --fix . ${filenames.join(' --file')}`,
+    `pnpm test -- --findRelatedTests ${filenames.join(' ')}`,
   ],
 }
