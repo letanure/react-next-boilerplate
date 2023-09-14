03f2687f28950297a849a04edc253932337a2e14
Luiz Tanure <letanure@gmail.com>
2023-09-15 01:04:10 +0200

    chore: Update linting and formatting configurations

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   // Lint & Prettify TS and JS files
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `pnpm lint --fix . ${filenames.join(' --file')}`,
+    `eslint ${filenames.join(' ')} --max-warnings=0 --fix`,
     `pnpm test -- --findRelatedTests ${filenames.join(' ')}`,
   ],
 }
