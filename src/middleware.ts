06d7ce7a97791ba81ab7a1e1f5b556c81c9c21fc
Luiz Tanure <letanure@gmail.com>
2023-09-15 01:09:34 +0200

    refactor(middleware): remove unused code and update config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,3 @@
-import createMiddleware from 'next-intl/middleware'
-
-export default createMiddleware({
-  locales: ['en', 'pt'],
-
-  defaultLocale: 'en',
-})
-
 export const config = {
   // Skip all paths that should not be internationalized. This example skips the
   // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
