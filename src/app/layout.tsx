671646d8ae39a6eb6721b992bcc2cadaaa78d953
Luiz Tanure <letanure@gmail.com>
2023-09-14 22:51:12 +0200

    refactor: Refactor layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,29 @@
+import StyledComponentsRegistry from '@/libs/registry'
+import type { Metadata } from 'next'
+
+import { Providers } from './providers'
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Boilerplate',
+    template: '%s | Boilerplate',
+  },
+  description: 'Next.js + Styled Components + TypeScript Boilerplate',
+  manifest: '/manifest.json',
+}
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return children
+  return (
+    <html lang="en">
+      <body>
+        <StyledComponentsRegistry>
+          <Providers>{children}</Providers>
+        </StyledComponentsRegistry>
+      </body>
+    </html>
+  )
 }
