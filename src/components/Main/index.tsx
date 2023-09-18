067b01ad3967e02026579b96e686c41cc71ea557
Luiz Tanure <letanure@gmail.com>
2023-09-18 21:23:30 +0200

    chore(ci): update linting and testing steps

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,10 +2,15 @@
 
 import * as S from './styles'
 
+type MainProps = {
+  title?: string
+  description?: string
+}
+
 const Main = ({
   title = 'Boilerplate',
   description = 'TypeScript, ReactJS, NextJS e Styled Components',
-}) => (
+}: MainProps) => (
   <S.Wrapper>
     <S.Title>{title}</S.Title>
     <S.Description>{description}</S.Description>
